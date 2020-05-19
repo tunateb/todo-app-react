f51f0769590b600c4a8fd37461274e3961c41ee6
tunateb <tunateb@gmail.com>
2020-05-19 13:12:13 +0300

    polish styles. add favicon and logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { faTrash } from "@fortawesome/free-solid-svg-icons";
 class App extends React.Component {
   state = {
     todo: "",
-    todos: ["CSS", "Javascript", "React"],
+    todos: ["Js", "Css", "HTML", "Angular", "jQuery"],
   };
 
   handleChange = (event) => {
@@ -22,30 +22,12 @@ class App extends React.Component {
   handleDelete = (props) => {
     this.state.todos.splice(props, 1);
     this.setState({ todos: this.state.todos });
-  }
+  };
 
   render() {
-    return (
-      <div className="container-sm mt-5 text-center">
-        <h3>You have {this.state.todos.length} tasks to complete!</h3>
-        <form onSubmit={this.handleSubmit}>
-          <div className="row justify-content-center mx-auto my-3 w-50 bg-secondary rounded">
-            <div className="form-group mt-3 col col-9 pr-0">
-              <input
-                type="text"
-                className="form-control"
-                id="todos-form"
-                placeholder="Enter a task..."
-                value={this.state.todo}
-                onChange={this.handleChange}
-              ></input>
-            </div>
-            <div className="col col-3 my-3 pl-0">
-              <button className="btn btn-success px-5">Add</button>
-            </div>
-          </div>
-        </form>
-        <div className="card m-auto w-50">
+    const renderList = () => {
+      if (this.state.todos.length > 0) {
+        return (
           <ul className="todo-list p-0 mb-0 text-light">
             {this.state.todos.map((todoItem, index) => {
               return (
@@ -53,14 +35,54 @@ class App extends React.Component {
                   key={index}
                   className="d-flex justify-content-between align-items-center"
                 >
-                  {todoItem}
-                  <button className="btn-sm" onClick={this.handleDelete}>
+                  <div className="ml-2">{todoItem}</div>
+
+                  <button
+                    className="btn-sm trash-btn"
+                    onClick={this.handleDelete}
+                  >
                     <FontAwesomeIcon icon={faTrash} />
                   </button>
                 </li>
               );
             })}
           </ul>
+        );
+      } else {
+        return (
+          <div className="card bg-warning text-center">
+            <h5>There are no tasks to do. Please add some...</h5>
+          </div>
+        );
+      }
+    };
+
+    return (
+      <div className="container-sm text-center">
+        <img className="logo" alt="logo" src={require("./img/logo.png")}></img>
+        <h3 className="title">
+          You have {this.state.todos.length} tasks to complete!
+        </h3>
+        <div className="card w-50 mx-auto mt-3 list-wrapper">
+          <form onSubmit={this.handleSubmit}>
+            <div className="row justify-content-center mt-3 rounded">
+              <div className="form-group mt-3 col col-9 pr-0">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="todos-form"
+                  placeholder="Enter a task..."
+                  value={this.state.todo}
+                  onChange={this.handleChange}
+                ></input>
+              </div>
+              <div className="col col-3 my-3 pl-0">
+                <button className="btn btn-success w-100">Add</button>
+              </div>
+            </div>
+          </form>
+
+          <div className="card">{renderList()}</div>
         </div>
       </div>
     );
